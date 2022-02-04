1340ab553c9ae85036ee31a9dec399eeb9921345
trX-X <tarinkumarpodavu4@gmail.com>
2022-02-04 22:27:43 +0530

    Added planets csv data

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+const PORT = process.env.PORT || 8000;
+const app = require("./app");
+
+const { loadPlanetsData } = require("./models/planets.model");
+
+const http = require("http");
+
+const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => console.log(`Listening on PORT : ${PORT}`));
+}
+
+startServer();
