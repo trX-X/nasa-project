d5741fe5e63de3c91bc57cacf3d7614cd426d988
trX-X <tarinkumarpodavu4@gmail.com>
2022-02-06 18:34:03 +0530

    Project completed(added Deleting of launches)

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
-  lauchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
   customers: ["NASA", "ZTM"],
   upcoming: true,
   success: true,
@@ -13,10 +15,37 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
 function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["ZeroTM", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
 };
