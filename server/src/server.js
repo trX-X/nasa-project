3400256a7abd5a4707f83c7684be6bb2e5b797f3
trX-X <tarinkumarpodavu4@gmail.com>
2022-02-20 21:58:53 +0530

    New PC, Updated with mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,20 @@
+const http = require("http");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://tarunp:ErppJKpNgYy5YjBb@cluster0.lsfg4.mongodb.net/nasa?retryWrites=true&w=majority";
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
-const http = require("http");
-
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => console.log('Mongo DB Connection ready'))
+
+mongoose.connection.on('error', (err) => console.error(err))
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => console.log(`Listening on PORT : ${PORT}`));
 }
